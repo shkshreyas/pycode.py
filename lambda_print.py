7272516a30822a37d70739f3fcecfebbdc3ebad6
Shreyas Kumar <94731699+shkshreyas@users.noreply.github.com>
2023-09-18 11:53:54 +0530

    Add files via upload

---
+++ lambda_print.py
@@ -0,0 +1,17 @@
+x= lambda a,b : a*b
+print(x(2,3))
+y=10.333365
+print("{:.2f}".format(y))
+print("%.2f" %round(y,2))
+print("{:.5f}".format(round(y,5)))
+print("it\'s raining")
+print("Hello\nWorld")
+print("Hello\tWorld")
+print(r"c:\new\temp\text.doc")
+print(f"value of {y}")
+a=c=2
+b=3
+print("sum of {} and {} is {}".format(a,b,a+b))
+print(id(a))
+print(id(c))
+print(type(a))
